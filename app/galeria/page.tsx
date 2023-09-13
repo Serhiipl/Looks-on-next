f4630283cac925e99e37ee35eb50ae0857dcd8da
Sergio_Mac <badzega@gmail.com>
2023-09-13 21:43:03 +0200

    ADD: galeria

--- app/galeria/page.tsx
+++ app/galeria/page.tsx
@@ -1,5 +1,54 @@
-import GalleryEl from "./GalleryEl";
+"use client";
+import React, { Component } from "react";
+import { render } from "react-dom";
+import SimpleGallery from "./SimpleGallery";
 
-export default function gallery() {
-  return <GalleryEl />;
+interface AppProps {}
+interface AppState {
+  name: string;
+}
+
+export default class App extends React.Component<AppProps, AppState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      name: "React",
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <SimpleGallery
+          galleryID="my-test-gallery"
+          images={[
+            {
+              largeURL:
+                "https://cdn.photoswipe.com/photoswipe-demo-images/photos/1/img-2500.jpg",
+              thumbnailURL:
+                "https://cdn.photoswipe.com/photoswipe-demo-images/photos/1/img-200.jpg",
+              width: 1875,
+              height: 2500,
+            },
+            {
+              largeURL:
+                "https://cdn.photoswipe.com/photoswipe-demo-images/photos/2/img-2500.jpg",
+              thumbnailURL:
+                "https://cdn.photoswipe.com/photoswipe-demo-images/photos/2/img-200.jpg",
+              width: 1669,
+              height: 2500,
+            },
+            {
+              largeURL:
+                "https://cdn.photoswipe.com/photoswipe-demo-images/photos/3/img-2500.jpg",
+              thumbnailURL:
+                "https://cdn.photoswipe.com/photoswipe-demo-images/photos/3/img-200.jpg",
+              width: 2500,
+              height: 1666,
+            },
+          ]}
+        />
+      </div>
+    );
+  }
 }
