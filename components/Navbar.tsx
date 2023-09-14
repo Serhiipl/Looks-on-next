fa3c22a236026d7b01cadb0b583d8c0a5052d26d
Sergio_Mac <badzega@gmail.com>
2023-09-14 21:56:29 +0200

    test commit

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     <nav className="nav-menu">
       <Link href="/">Glówna</Link>
       <Link href="/">O nas</Link>
-      <Link href="/">Oferta</Link>
+      <Link href="/">Usługi</Link>
       <Link href="/galeria">Galeria</Link>
       <Link href="/">Kontakt</Link>
     </nav>
