618a04c499940401f4577c4cb38b387e51c52a5b
Sergio_Mac <badzega@gmail.com>
2023-10-21 20:44:24 +0200

    ADD: styles for the errors

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -9,6 +9,11 @@ export default function ContactForm() {
   const [formEmail, setformEmail] = useState("");
   const [formTel, setformTel] = useState("");
   const [formMessage, setformMessage] = useState("");
+  const [nameError, setNameError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [telError, setTelError] = useState(false);
+  const [messageError, setMessageError] = useState(false);
+
   //  Clear form fields...
   function clearFormFields() {
     setformName("");
@@ -45,22 +50,73 @@ export default function ContactForm() {
   function handleSubmit(e: FormEvent) {
     // Prevent the browser from reloading the page
     e.preventDefault();
-    // Виконати валідацію полів
+
+    // Оголошуємо змінні для валідації телефону і електронної пошти
+    let telValid = true;
+    let emailValid = true;
+
+    // Clear previous errors
+    setNameError(false);
+    setMessageError(false);
+    setEmailError(false);
+    setTelError(false);
+
+    // Performing field validation
+    if (!name) {
+      setNameError(true);
+    }
+
+    if (!message) {
+      setMessageError(true);
+    }
+
+    if (!tel) {
+      setTelError(true);
+    }
+
     if (!name || !message) {
       // Якщо поля formName або formMessage порожні, вивести помилку
-      alert('Поля "Imie" та "Wiadomość" є обовʼязковими');
+      //   alert('Поля "Imie" та "Wiadomość" є обовʼязковими');
       return;
     }
     if (!email && !tel) {
       // Якщо обидва поля formEmail і formTel порожні, вивести помилку
+
       alert('Потрібно заповнити хоча б одне з полів "Email" або "Telefon"');
       return;
     }
+
+    // if (/^[\d()+ -]{6,14}$/.test(formTel) && formEmail === "") {
+    //   console.log("Telefon OK");
+    //   telValid = true;
+    //   setTelError(false);
+    // } else if (/^[\d()+ -]{6,14}$/.test(formTel) && formEmail !== "") {
+    //   console.log("Telefon NOT OK");
+    //   telValid = false;
+    //   setTelError(true);
+    // }
+
+    // if (
+    //   /^[\w+.-]+@\w+([.-]?\w+)*(\.\w{2,8})+$/.test(formEmail) &&
+    //   formTel === ""
+    // ) {
+    //   console.log("Email OK");
+    //   emailValid = true;
+    //   setEmailError(false);
+    // } else if (
+    //   /^[\w+.-]+@\w+([.-]?\w+)*(\.\w{2,8})+$/.test(formEmail) &&
+    //   formTel !== ""
+    // ) {
+    //   console.log("Email NOT OK");
+    //   emailValid = false;
+    //   setEmailError(true);
+    // }
 
     if (/^[\d()+ -]{6,14}$/.test(formTel) || formEmail !== "") {
       console.log("Telefon OK");
     } else {
       console.log("Telefon NOT OK");
+      setTelError(true);
     }
 
     if (
@@ -70,51 +126,9 @@ export default function ContactForm() {
       console.log("Email OK");
     } else {
       console.log("Email NOT OK");
+      setEmailError(true);
     }
 
-    // if (!email && !tel) {
-    //   // Якщо обидва поля formEmail і formTel порожні, вивести помилку
-    //   alert("Należy wypełnić co najmniej jedno z pól „E-mail” lub „Telefon”.");
-    //   return;
-    // }
-    // if (!name || !message) {
-    //   // Якщо поля formName або formMessage порожні, вивести помилку
-    //   alert("Pola „Imie” i „Wiadomość” są obowiązkowe");
-    //   return;
-    // }
-
-    // let emailValid = true;
-    // let telValid = true;
-
-    // if (!email && !tel) {
-    //   // Якщо обидва поля formEmail і formTel порожні, вивести помилку
-    //   alert('Потрібно заповнити хоча б одне з полів "Email" або "Telefon"');
-    //   emailValid = false;
-    //   telValid = false;
-    // } else {
-    //   if (/^[\d()+ -]{6,14}$/.test(formTel) && formEmail == "") {
-    //     console.log("Telefon OK");
-    //     // telValid = true;
-    //   } else if (/^[\d()+ -]{6,14}$/.test(formTel) && formEmail !== "") {
-    //     console.log("Telefon NOT OK");
-    //     telValid = false;
-    //   }
-
-    //   if (
-    //     /^[\w+.-]+@\w+([.-]?\w+)*(\.\w{2,8})+$/.test(formEmail) &&
-    //     formTel == ""
-    //   ) {
-    //     console.log("Email OK");
-    //     // emailValid = true;
-    //   } else if (
-    //     /^[\w+.-]+@\w+([.-]?\w+)*(\.\w{2,8})+$/.test(formEmail) &&
-    //     formTel !== ""
-    //   ) {
-    //     console.log("Email NOT OK");
-    //     emailValid = false;
-    //   }
-    // }
-    // if (isValidForm() && (emailValid || telValid)) {
     if (isValidForm()) {
       // Read the form data
       const form = e.target as HTMLFormElement;
@@ -139,8 +153,12 @@ export default function ContactForm() {
         <div className="form-item">
           <label htmlFor="formName"></label>
           <input
+            className={"form-input " + (nameError ? "error" : "")}
             value={formName}
-            onChange={(e) => setformName(e.target.value)}
+            onChange={(e) => {
+              setformName(e.target.value);
+              setNameError(false);
+            }}
             type="text"
             name="formName"
             autoComplete="on"
@@ -151,6 +169,7 @@ export default function ContactForm() {
         <div className="form-item">
           <label htmlFor="formEmail"></label>
           <input
+            className={"form-input " + (emailError ? "error" : "")}
             value={formEmail}
             onChange={(e) => {
               setformEmail(e.target.value);
@@ -165,9 +184,11 @@ export default function ContactForm() {
         <div className="form-item">
           <label htmlFor="formTel"></label>
           <input
+            className={"form-input " + (telError ? "error" : "")}
             value={formTel}
             onChange={(e) => {
               setformTel(e.target.value);
+              setTelError(false);
             }}
             type="tel"
             name="formTel"
@@ -179,9 +200,11 @@ export default function ContactForm() {
         <div className="form-item">
           <label htmlFor="formMessage"></label>
           <textarea
+            className={"form-input " + (messageError ? "error" : "")}
             value={formMessage}
             onChange={(e) => {
               setformMessage(e.target.value);
+              setMessageError(false);
             }}
             name="formMessage"
             id="formMessage"
